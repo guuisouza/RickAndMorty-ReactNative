10d0ebc8a2ef3d03feb8c9f8eb2ac7cf3aa5565d
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-04-11 18:42:34 -0300

    Corrigido Rotas e adicionado rota de Cards

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,64 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from './pages/login';
+import Register from './pages/register';
+import Cards from './pages/cards';
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
-    <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="login"
-            component={Login}
-            options={{
-              title: 'LOGIN',
-              headerTitleAlign: 'center',
-              headerStyle: {
-                backgroundColor: '#03DBCA',
-              },
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                color: '#fff',
-              },
-            }}
-            />
-        </Stack.Navigator>
-    </NavigationContainer>
+    return (
+
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="login"
+                    component={Login}
+                    options={{
+                        title: 'LOGIN',
+                        headerTitleAlign: 'center',
+                        headerStyle: {
+                            backgroundColor: '#03DBCA',
+                        },
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                            color: '#fff',
+                        },
+                    }}
+                />
+                <Stack.Screen
+                    name="register"
+                    component={Register}
+                    options={{
+                        title: 'CADASTRO',
+                        headerTitleAlign: 'center',
+                        headerStyle: {
+                            backgroundColor: '#03DBCA',
+                        },
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                            color: '#fff',
+                        },
+                    }}
+                />
+                <Stack.Screen
+                    name="cards"
+                    component={Cards}
+                    options={{
+                        title: 'Rick And Morty CARDS',
+                        headerTitleAlign: 'center',
+                        headerStyle: {
+                            backgroundColor: '#03DBCA',
+                        },
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                            color: '#fff',
+                        },
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
