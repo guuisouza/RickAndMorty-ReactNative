8187da790342f9bbbcb0844cbce71900f99fa952
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-04-11 15:39:09 -0300

    Criado página de login + styles

---
+++ src/pages/login.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const Login = () => {
+    // Variáveis de estado para armazenar email e senha
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const navigation = useNavigation()
+
+    // Verifica campo email e senha, se estiver correto navega até os cards
+    const handleLogin = () => {
+        if (email === '' && password === ''){
+            navigation.navigate('cards')
+        } else {
+            alert('E-mail ou senha inválidos!')
+        }
+    }
+
+    // Navega até a página de registro
+    const handleRegister = () => {
+        navigation.navigate('register')
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={ styles.input }
+                placeholder="E-mail"
+                value={email}    
+                onChangeText={setEmail}
+            />
+            <TextInput
+                style={ styles.input }
+                placeholder="Senha"
+                secureTextEntry={true}
+                value={password}    
+                onChangeText={setPassword}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleLogin} >
+                <Text style={ styles.buttonText }>Entrar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleRegister} >
+                <Text style={ styles.buttonText }>Cadastrar Usuário</Text>
+            </TouchableOpacity>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginVertical: 10,
+        width: '80%',
+    },
+    button: {
+        backgroundColor: "#3498db",
+        borderRadius: 5,
+        padding: 10,
+        width: '80%',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 18,
+    }
+})
+
+export default Login;
